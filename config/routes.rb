d3a8deb3bfad2d184014d682a51dd27522652e0d
Reyes Montemayor <sjpete1234@hotmail.com>
2018-05-21 14:23:44 +0900

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'new', to: 'games#new', as: :new
+  post 'score', to: 'games#score', as: :score
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Prefix Verb URI Pattern      Controller#Action
+#    new GET  /new(.:format)   games#new
+#  score POST /score(.:format) games#score
